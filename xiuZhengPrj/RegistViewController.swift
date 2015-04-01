7a31a0569a22a7ed18381c8bffafa6636831df23
hux <hux@huxdeMac.local>
2015-03-31 18:45:24 -0700

    uipickerview

--- xiuZhengPrj/RegistViewController.swift
+++ xiuZhengPrj/RegistViewController.swift
@@ -12,12 +12,15 @@ class RegistViewController: UIViewController {
 
     @IBOutlet weak var phoneNumTf: UITextField!
     @IBOutlet weak var checkCodeTf: UITextField!
-    
     @IBOutlet weak var passwordTf: UITextField!
+    
+    var colors = ["Red","Yellow","Green","Blue"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +31,6 @@ class RegistViewController: UIViewController {
     @IBAction func phoneEditEnd(sender: AnyObject) {
         if self.phoneNumTf.text == "" {
             self.phoneNumTf.text = "请填写正确的手机号"
-            
-            
-            
         }
     }
 
@@ -47,5 +47,17 @@ class RegistViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int)->Int {
+        return colors.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return colors[row]
+    }
 
 }
